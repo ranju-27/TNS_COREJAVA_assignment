9e514e378a4901aed43cf14736c16501a035632d
ranju-27 <ranjithrv27@gmail.com>
2025-08-20 15:43:47 +0530

    Add files via upload

---
+++ src/assignment3/com/kiruthiga/assignment/Book.java
@@ -0,0 +1,19 @@
+package assignment_three.com.kiruthiga.assignment;
+
+public class Book extends Item {
+    private String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: [ID: " + id + ", Title: " + title + ", Author: " + author + "]");
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
